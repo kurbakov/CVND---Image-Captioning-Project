74639423179a68aa5cc19f097943e3498b3c6a02
kurbakov <kurbakov.d@gmail.com>
2019-04-21 18:02:15 +0200

    Test the local env for the project

--- model.py
+++ model.py
@@ -23,10 +23,16 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
+        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers)
+        self.linear = nn.Linear(in_features=hidden_size, out_features=vocab_size)
     
     def forward(self, features, captions):
-        pass
+        embedding_out = self.embedding(captions[:,:-1])
+        embedding_out = torch.cat((features.unsqueeze(1), embedding_out), 1)
+        lstm_out, _ = self.lstm(embedding_out)
+        return self.linear(lstm_out)
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
